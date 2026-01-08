dc4f958edfbc93fb8e20ac7da05aec7d1274f8ee
Vadym Vynokur <king.krollie@gmail.com>
2026-01-08 19:23:32 +0200

    fix

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.14-slim
+FROM python:3.12-slim
 LABEL authors="king.krollie@gmail.com"
 
 ENV PYTHONUNBUFFERED=1
@@ -7,7 +7,7 @@ WORKDIR /app
 
 COPY requirements.txt .
 
-RUN pip install -r requirements.txt
+RUN pip install -r --no-cache-dir requirements.txt
 
 COPY app/ .
 
--- app/main.py
+++ app/main.py
@@ -1,34 +1,40 @@
 import os
 import requests
 
-API_KEY = os.getenv("API_KEY")
 CITY = "Paris"
 URL = "https://api.weatherapi.com/v1/current.json"
 
-
 def get_weather() -> None:
-    if not API_KEY:
-        print("Error: API_KEY environment variable is not set")
-        return
+    try:
+        api_key = os.getenv("api_key")
+        if not api_key:
+            print("Error: api_key environment variable is not set")
+            return
 
-    params = {
-        "key": API_KEY,
-        "q": CITY,
-        "lang": "en",
-    }
+        params = {
+            "key": api_key,
+            "q": CITY,
+            "lang": "en",
+        }
 
-    response = requests.get(URL, params=params, timeout=10)
-    response.raise_for_status()
+        response = requests.get(URL, params=params, timeout=10)
+        response.raise_for_status()
 
-    data = response.json()
+        data = response.json()
 
-    print(
-        f"{data['location']['name']}/{data['location']['country']} "
-        f"{data['location']['localtime']} "
-        f"Weather: {data['current']['temp_c']} Celsius, "
-        f"{data['current']['condition']['text']}"
-    )
+        print(
+            f"{data['location']['name']}/{data['location']['country']} "
+            f"{data['location']['localtime']} "
+            f"Weather: {data['current']['temp_c']} Celsius, "
+            f"{data['current']['condition']['text']}"
+        )
 
+    except requests.exceptions.RequestException as e:
+        print(f"Network/API error: {e}")
+    except ValueError:
+        print("Error: Failed to parse JSON response from API.")
+    except KeyError as e:
+        print(f"Error: Missing expected data in API response: {e}")
 
 if __name__ == "__main__":
     get_weather()
